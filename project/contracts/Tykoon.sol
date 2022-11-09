9eaac853f36f50a3d3ec4cdd81b510e867c474c9
Ridwan Maulana Tanjung <rdtanx@gmail.com>
2022-11-09 12:41:24 +0700

    fix syntax error

--- project/contracts/Tykoon.sol
+++ project/contracts/Tykoon.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 
 import "chiru-labs/ERC721A@4.2.3/contracts/ERC721A.sol";
 import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/access/Ownable.sol";
-import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/token/ERC20/IERC20.sol";"
+import "OpenZeppelin/openzeppelin-contracts@4.7.3/contracts/token/ERC20/IERC20.sol";
 
 contract Tykoon is ERC721A, Ownable {
     IERC20 public idrt;
@@ -39,12 +39,16 @@ contract Tykoon is ERC721A, Ownable {
             totalSupply() + quantity <= _maxSupply,
             "Not enough NFTs to mint, reached maximum supply"
         );
-        bool tfSucceed = idrt.transferFrom(msg.sender,address(this), quantity*_idrtPricePerNFT);
-        require(tfSucceed,"Failed to make USDT transfer transaction");
+        bool tfSucceed = idrt.transferFrom(
+            msg.sender,
+            address(this),
+            quantity * _idrtPricePerNFT
+        );
+        require(tfSucceed, "Failed to make USDT transfer transaction");
         _safeMint(msg.sender, quantity);
     }
 
-    function withdrawIDRT(uint256 amount, address to) external onlyOwner{
-        idrt.transferFrom(address(this),to, amount);
+    function withdrawIDRT(uint256 amount, address to) external onlyOwner {
+        idrt.transferFrom(address(this), to, amount);
     }
 }
